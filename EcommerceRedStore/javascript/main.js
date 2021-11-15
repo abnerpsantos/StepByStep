d9e305d6934b494068e21bf4ceb5843e91ab2aac
abnerpsantos <abner.saan09@gmail.com>
2021-11-15 11:48:58 -0300

    Finished
    An e-commerce website called: RedStore

---
+++ EcommerceRedStore/javascript/main.js
@@ -0,0 +1,46 @@
+// New
+var Indicator = document.getElementById("indicator");
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+
+function register() {
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(100px)"
+}
+
+function login() {
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+    Indicator.style.transform = "translateX(0px)";
+}
+
+var MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+    if (MenuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px";
+    } else {
+        MenuItems.style.maxHeight = "0px";
+    }
+}
+
+// New JS code for product gallery
+
+var productImg = document.getElementById("productImg");
+var smallImg = document.getElementsByClassName("smallImg");
+
+smallImg[0].onclick = function () {
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function () {
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function () {
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function () {
+    productImg.src = smallImg[3].src;
+}
